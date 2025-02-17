afa108da6d21ed3f052d5a08522c06c92cd6a714
Gustavo Henrique Montanari <gustavo.montanari22@gmail.com>
2025-02-17 06:59:45 -0300

    Retornando a C, AEDS primeiro semestre 17/02

---
+++ Esqueleto.c
@@ -0,0 +1,11 @@
+#include <stdio.h> /*bibliotecas*/
+#include <stdlib.h> /*bibliotecas*/
+
+
+/*Corpo basica de um programa em C, todo programa tera de haver um main, com parenteses e chaves, alem do return 0 no final.*/
+
+int main () {
+    printf("Hello World"); /*Comando de aparecer na tela*/
+    system("pause");
+    return 0;
+}
---
+++ Variaveis.c
@@ -0,0 +1,3 @@
+#include <stdio.h> /*bibliotecas*/
+#include <stdlib.h> /*bibliotecas*/
+
