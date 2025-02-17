89b416cd434c0c3cb9f17fedbe3dfaba9d3402e1
Gustavo Henrique Montanari <gustavo.montanari22@gmail.com>
2025-02-17 07:43:43 -0300

    Update Variaveis.c

--- Variaveis.c
+++ Variaveis.c
@@ -1,3 +1,18 @@
 #include <stdio.h> /*bibliotecas*/
 #include <stdlib.h> /*bibliotecas*/
+/*Uma variavel eh composta por
+    tipo nome;
+    com 4 tipos basicos
+    char = caracteres
+    int = inteiros 
+    float = ponto flutuante numeros reais
+    double = maior incisao em numeros reais
+    key sensitive = maiusculo e minusculo sao diferentes*/
 
+int main () {
+    int A,B,C,D;
+    char letra = 'a';
+    char letrab = 10;
+    float E = 5.25;
+    double F = 5.0e3 /*5x10 ao cubo*/
+}
