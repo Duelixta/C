ae6d2ee15c987ab76bd7696f76c7915752f4cd2a
Gustavo Henrique Montanari <gustavo.montanari22@gmail.com>
2025-02-23 07:07:47 -0300

    Operadores

--- AtvdsOperadores4.c
+++ AtvdsOperadores4.c
@@ -11,22 +11,4 @@ mostre:
 4. Faça um programa que receba dois números maiores que zero, calcule e mostre
 um elevado ao outro.
 */
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 
-int main() {
-int a,quad,cub;
-double raizqd,raizcub;
-printf("Me de um numero maior que zero, para calcular");
-scanf("%d",&a);
-
-    quad = pow(a,2);
-    cub = pow(a,3);
-    raizqd = sqrt(a);
-    raizcub = cbrt(a); /*Raiz cubica eh cbrt*/
-
-printf("O resultado das operacoes com o numero %d, sao\n seu quadrado eh %d, seu cubico eh %d, sua raiz qdr eh %.2lf e sua raiz cubica eh %.2lf",a,quad,cub,raizqd,raizcub);
-
-return 0;
-}
